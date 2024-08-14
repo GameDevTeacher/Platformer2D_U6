b49719f8e98e82e49fc3e49aed3ebd8d2b00afc1
Markus Lange <markus@gamedevteacher.com>
2024-08-14 23:12:59 +0200

    Updates

--- Assets/Scripts/Player/PlayerHealthController.cs
+++ Assets/Scripts/Player/PlayerHealthController.cs
@@ -12,13 +12,11 @@ public class PlayerHealthController : MonoBehaviour
         {
             if (i < target.playerHealth)
             {
-                hearts[i].color =
-                    new Color(1,1,1,1);
+                hearts[i].color = new Color(1,1,1,1);
             }
             else
             {
-                hearts[i].color =
-                    new Color(1, 1, 1, 0.5f);
+                hearts[i].color = new Color(1, 1, 1, 0.5f);
             }
         }
     }
